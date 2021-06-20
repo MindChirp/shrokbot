5b839bedce77fbdcc519860622f84abfaa2fb927
MindChirp <33499052+MindChirp@users.noreply.github.com>
2021-06-21 01:00:20 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,72 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const fs = require("fs");
+const {prefix, token} = require('./config.json');
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+
+	client.commands.set(command.name, command);
+}
+
+
+
+client.once("ready", () => {
+    console.log("Ready");
+})
+
+client.login(token);
+
+
+
+
+
+client.on("message", async message => {
+
+
+    if(!message.guild) return;
+
+
+        if(message.content == "#donkey") {
+            var voiceChannel = message.member.voice.channel;
+            voiceChannel.join().then(connection => {
+              const dispatcher = connection.play('donkey.mp3', {
+                  volume: 1,
+              })
+    
+              dispatcher.on('finish', end => {
+                
+                  connection.disconnect();
+                  dispatcher.destroy();
+                });
+            }).catch(err => console.log(err))
+        }
+
+
+
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+        
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
+        const command = args.shift().toLowerCase();
+
+        if (!client.commands.has(command)) return;
+
+        try {
+            client.commands.get(command).execute(message, args);
+        } catch (error) {
+            console.error(error);
+            message.reply('there was an error trying to execute that command!');
+        }
+
+            
+
+})
+
+
+
