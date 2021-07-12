806da38dac801e55a90b7aab624330d485156fae
gap41 <76493717+gap41@users.noreply.github.com>
2021-07-12 14:34:19 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,13 @@ module.exports = {
     description: "Gives you help",
     execute(message, args) {
 
-        message.channel.send("```**Complete list of commands** \n " + prefix + "topic - gives you a random topic \n " + prefix + "addtopic - adds a topic to the topic list \n " + prefix + "banword - bans a word (or a word / sentence similar to it) from being added to the topic list \n " + prefix + "donkey - DONKEEEEY \n " + prefix + "siwallah - wallaaaah```");
+        message.channel.send("```**Complete list of commands** \n "
+                             + prefix + "topic - gives you a random topic \n "
+                             + prefix + "addtopic - adds a topic to the topic list \n "
+                             + prefix + "banword - bans a word (or a word / sentence similar to it) from being added to the topic list \n "
+                             + prefix + "donkey - DONKEEEEY \n "
+                             + prefix + "siwallah - wallaaaah \n
+                             + prefix + "rick - Never gonna give you up```");
 
     }
 }
